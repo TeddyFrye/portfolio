3c30a7273e674bb5ce0441414166f5211a1ccd4f
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2024-09-18 15:08:50 -0500

    Changes vite config to work with netlify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 // vite.config.js
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   root: ".",
+//   server: {
+//     port: 5173,
+//   },
+// });
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: ".",
-  server: {
-    port: 5173,
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
 });
